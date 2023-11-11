0577be4c8eb7f7c7304e0e2fb495205eaedbfe35
Peter Harvey <peter@csure.me>
2023-11-11 20:26:45 +0000

    fixed deploy.sh for new file structure

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,19 @@
+cd /workspaces/core/config/custom_components/manage_energy
 git add .
 git commit -m "$1"
 git push origin main
-ssh root@10.0.4.235 'cp -r /config/custom_components/manage_energy/* /config/manage_energy_old'
-scp -r /workspaces/core/config/custom_components/manage_energy/* root@10.0.4.235:/config/custom_components/manage_energy/
+ssh root@10.0.4.235 'cp -r /root/homeassistant/custom_components/manage_energy/* /root/homeassistant/config/manage_energy_old'
+if [ $? -ne 0 ]; then
+    echo "Backup failed. Exiting."
+    exit 1
+fi
+
+scp -r /workspaces/core/config/custom_components/manage_energy/* root@10.0.4.235:/root/homeassistant/custom_components/manage_energy/
+if [ $? -ne 0 ]; then
+    echo "Copy failed. Exiting."
+    exit 1
+fi
+
 ssh root@10.0.4.235 'ha core restart'&
+
+
