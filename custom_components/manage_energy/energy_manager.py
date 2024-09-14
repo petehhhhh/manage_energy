0996a8df3e38669c591e1fc5010d818765b35c84
petehhhhh <40730407+petehhhhh@users.noreply.github.com>
2024-09-14 10:01:53 +1000

    Update energy_manager.py
    Improved Tesla error handling

--- custom_components/manage_energy/energy_manager.py
+++ custom_components/manage_energy/energy_manager.py
@@ -201,7 +201,7 @@ class manage_energy ():
             await self.update_status("Error in Tesla_Charging. Error : " + str(e))
             error_message = traceback.format_exc()
     # Log the error with the traceback
-            _LOGGER.error(f"Error in Tesla_Charging: {error_message}")
+            _LOGGER.error(f"Error in Tesla_Charging: {str(e)}. Traceback: {error_message}")
             return False
         
 
