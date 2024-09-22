06b26a5271ebc6b2ff4a4a1db712a928b9217a91
petehhhhh <40730407+petehhhhh@users.noreply.github.com>
2024-09-22 10:18:31 +1000

    Update energy_manager.py

--- custom_components/manage_energy/energy_manager.py
+++ custom_components/manage_energy/energy_manager.py
@@ -165,7 +165,7 @@ class manage_energy:
                 == "on"
             )
             tesla_home = (
-                self._hass.states.get("binary_sensor.pete_s_tesla_presence.state").state == "on"
+                self._hass.states.get("binary_sensor.pete_s_tesla_presence").state == "on"
             )
             charge_limit = int(
                 self._hass.states.get("number.pete_s_tesla_via_fleet_charge_limit").state
