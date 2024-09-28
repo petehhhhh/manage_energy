ff2a171347d9ab90e095f28e3417212cc9458f9e
petehhhhh <40730407+petehhhhh@users.noreply.github.com>
2024-09-28 15:49:56 +1000

    Update energy_manager.py

--- custom_components/manage_energy/energy_manager.py
+++ custom_components/manage_energy/energy_manager.py
@@ -202,7 +202,7 @@ class manage_energy:
                 charge_amps = 16
             else:
                 if self.actuals.feedin <= self._cheap_price:
-                    charge_amps = round(self.actuals.excess_energy * 1000 / 240 / 3, 0) + current_charge
+                    charge_amps = round(self.actuals.excess_energy * 1000 / 240 / 3, 0) + current_amps
                 else:
                     charge_amps = 0
 
